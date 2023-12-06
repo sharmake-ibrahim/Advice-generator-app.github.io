767cb815e04791cc78b5823001944f6135b3c2ae
Sharmake Ahmed <sharmakeahmedibrahim@gmail.com>
2023-12-07 02:38:04 +1300

    I have added some js while using Api

--- Scss/main.js
+++ Scss/main.js
@@ -2,20 +2,19 @@
     const Api = "https://api.adviceslip.com/advice";
    
  function Advice_Generator() {
-
+        const Container = document.querySelector('.Container')
         const Icon_dice = document.querySelector('.icon-dice');
-        // const AdviceID = document.querySelector('p');
-        // const Quotes= document.querySelector('h4');
+        const AdviceID = document.querySelector('p');
+        const Quotes= document.querySelector('h4');
 
+            console.log(Container);
         const getAdvice = ()=> {
         fetch(Api)
         .then( (response) => response.json())
         .then((data)=> {
-            console.log(data)
-            const AdviceID = document.querySelector('p');
-            const Quotes= document.querySelector('h4');
-            AdviceID.textContent =  " Advice # " + data.slip.id;
-            Quotes.textContent = data.slip.advice;
+            // console.log(data)
+            AdviceID.textContent = "Advice" +" #" +`${data.slip.id}`;
+            Quotes.textContent = `${data.slip.advice}`;
 
           
         })
@@ -24,7 +23,7 @@
 
 
         Icon_dice.addEventListener('click', ()=> {
-        getAdvice();
+                getAdvice();
         })
  }
 
